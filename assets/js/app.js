14e895f4bb5b5e2b5d792a4f472282036bb5b185
Ayda Sulca <suni_yuri_94@hotmail.com>
2017-06-01 21:23:30 -0500

    carousel-avance-1

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+$('.container').unslider();
+$('.endeos-unslider').unslider({
+  speed: 500,
+  delay: 3000,
+  complete: function(){},
+  keys: true,
+  dots: true,
+  fluid: true
+});
+});
