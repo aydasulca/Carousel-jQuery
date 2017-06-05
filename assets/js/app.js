c9e6aad50f5843247dd5d7cea5648d59f755d856
Ayda Sulca <suni_yuri_94@hotmail.com>
2017-06-04 22:42:56 -0500

    slider avance-1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,43 @@
-$( "#right" ).click(function() {
-  $( ".galeria-slider" ).animate({ "left": "+=350px" }, "slow" );
-});
+$( () => {
+//almacenar slider en una variable
+var slider = $('#slider');
+//almacenar botones
+var siguiente = $('#btn-next');
+var anterior = $('#btn-prev');
+//mover ultima imagen al primer lugar
+$('#slider section:last').insertBefore('#slider section:first');
 
-$( "#left" ).click(function(){
-  $( ".galeria-slider" ).animate({ "left": "-=350px" }, "slow" );
-});
+//mostrar la primera imagen con un margen de -100%
+slider.css('margin-left', '-'+100+'%');
+
+function moverD(){
+  slider.animate({
+    marginLeft:'-'+200+'%'
+  } , 700, function(){
+    $('#slider section:first').insertAfter('#slider section:last');
+    slider.css('margin-left', '-'+100+'%');
+  });
+}
+
+function moverI(){
+  slider.animate({
+    marginLeft:0
+  } , 700, function(){
+    $('#slider section:last').insertBefore('#slider section:first');
+    slider.css('margin-left', '-'+100+'%');
+  });
+}
+siguiente.on('click', function(){
+  moverD();
+})
+anterior.on('click', function(){
+  moverI();
+})
+
+function autoSlide() {
+  setInterval(function() {
+    moverD();
+  }, 5000);
+} autoSlide();
+
+})
